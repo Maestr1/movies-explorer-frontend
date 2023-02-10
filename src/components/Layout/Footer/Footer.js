acb9e0f8c16508babebacefaa24aafe33b4f50f1
Maestr1 <maestr234@gmail.com>
2023-02-10 16:09:49 +0300

    feat: сделан pixel-perfect Homepage

--- src/components/Layout/Footer/Footer.js
+++ src/components/Layout/Footer/Footer.js
@@ -1,9 +1,23 @@
 import React from 'react';
 import './Footer.css';
+import {Link} from 'react-router-dom';
 
 function Footer(props) {
   return (
-    <footer>Footer</footer>
+    <footer className="footer container">
+      <h2 className="footer__title">Учебный проект Яндекс.Практикум х BeatFilm.</h2>
+      <div className="footer__wrapper">
+        <p className="footer__date">&copy; 2023</p>
+        <ul className="footer__links">
+          <li>
+            <Link to="https://practicum.yandex.ru/" className="footer__link link">Яндекс Практикум</Link>
+          </li>
+          <li>
+            <Link to="https://github.com/Maestr1/" className="footer__link link">Github</Link>
+          </li>
+        </ul>
+      </div>
+    </footer>
   );
 }
 
