da5aa5ad4bec7c2e03330f18109e38993adf714a
Maestr1 <maestr234@gmail.com>
2023-02-10 14:50:05 +0300

    feat: добавлен Portfolio

--- src/components/Homepage/Portfolio/Portfolio.js
+++ src/components/Homepage/Portfolio/Portfolio.js
@@ -1,8 +1,23 @@
 import React from 'react';
+import './Portfolio.css';
+import {Link} from 'react-router-dom';
 
 function Portfolio(props) {
   return (
-    <section>Portfolio</section>
+    <section className="portfolio container">
+      <h2 className="portfolio__title">Портфолио</h2>
+      <ul className="portfolio__list">
+        <li className="portfolio__item">
+          <Link className="portfolio__link link" to="#">Статичный сайт</Link>
+        </li>
+        <li className="portfolio__item">
+          <Link className="portfolio__link link" to="#">Адаптивный сайт</Link>
+        </li>
+        <li className="portfolio__item">
+          <Link className="portfolio__link link" to="#">Одностраничное приложение</Link>
+        </li>
+      </ul>
+    </section>
   );
 }
 
