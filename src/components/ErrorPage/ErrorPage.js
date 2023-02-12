bdbbd43868826a849bcd7ae98a09ddf498996203
Maestr1 <maestr234@gmail.com>
2023-02-13 00:28:38 +0300

    feat: добавлена кнопка "назад" в 404-page

--- src/components/ErrorPage/ErrorPage.js
+++ src/components/ErrorPage/ErrorPage.js
@@ -1,13 +1,15 @@
-import { Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import "./ErrorPage.css"
 
 export default function ErrorPage() {
+
+  let navigate = useNavigate()
 
   return (
     <div className="error-page">
       <h1 className="error-page__title">404</h1>
       <h2 className="error-page__subtitle">Страница не найдена</h2>
-      <Link aria-label="Сылка на предыдущую страницу" className="error-page__back-link link" to='/'>Назад</Link>
+      <button onClick={() => navigate(-1)} aria-label="Сылка на предыдущую страницу" className="error-page__back-link btn">Назад</button>
     </div>
   );
 }
