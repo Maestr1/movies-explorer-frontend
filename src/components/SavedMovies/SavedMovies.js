4c031cb6a4191478901309732b2c7bfaf3663f1e
Maestr1 <maestr234@gmail.com>
2023-02-16 17:50:32 +0300

    fix: добавлена форма поиска в сохраненные, исправлена сетка в techs

--- src/components/SavedMovies/SavedMovies.js
+++ src/components/SavedMovies/SavedMovies.js
@@ -1,15 +1,20 @@
 import React from 'react';
-import './SavedMovies.css'
+import './SavedMovies.css';
 import MoviesCard from '../Movies/MoviesCard/MoviesCard';
+import SearchForm from '../SearchForm/SearchForm';
+
 function SavedMovies(props) {
   return (
-    <section className="moviesCardList container">
-      <ul className="moviesCardList__list">
-        <MoviesCard btnType='remove'/>
-        <MoviesCard btnType='remove'/>
-        <MoviesCard btnType='remove'/>
-      </ul>
-    </section>
+    <>
+      <SearchForm/>
+      <section className="moviesCardList container">
+        <ul className="moviesCardList__list">
+          <MoviesCard btnType="remove"/>
+          <MoviesCard btnType="remove"/>
+          <MoviesCard btnType="remove"/>
+        </ul>
+      </section>
+    </>
   );
 }
 
